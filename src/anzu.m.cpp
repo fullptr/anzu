ef316b3c7d920b5faecfac2c9f0ffca33b9feca4
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2022-01-04 18:57:41 +0000

    Add symbol table

--- src/anzu.m.cpp
+++ src/anzu.m.cpp
@@ -5,6 +5,7 @@
 #include <string_view>
 #include <ranges>
 #include <vector>
+#include <unordered_map>
 
 #include <sstream> // Temp
 
@@ -13,26 +14,32 @@ bool is_int(const std::string& token)
     return token.find_first_not_of( "0123456789" ) == std::string::npos;
 }
 
-struct scope
+int fetch_int(const std::string& token, const std::unordered_map<std::string, int>& symbols)
 {
-    // To be used later. Scopes will be used to store named values. Function calls
-    // will create new scopes. This is essentially a stack frame.
-};
+    if (is_int(token)) {
+        return std::stoi(token);
+    } else if (symbols.contains(token)) {
+        return symbols.at(token);
+    } else {
+        fmt::print("Error: Unknown int");
+        std::exit(1);
+    }
+}
 
-constexpr auto OP_PRINT    = std::string_view{"pr"};
-constexpr auto OP_PUSH_INT = std::string_view{"pi"};
-constexpr auto OP_ADD      = std::string_view{"+"};
+constexpr auto OP_PRINT     = std::string_view{"pr"};
+constexpr auto OP_PUSH_INT  = std::string_view{"pi"};
+constexpr auto OP_ADD       = std::string_view{"+"};
+constexpr auto OP_STORE_INT = std::string_view{"si"};
 
 int main()
 {
     anzu::value_stack vs;
 
-    // The stack of scopes for names values.
-    //std::vector<scope> scopes;
+    std::unordered_map<std::string, int> symbol_table;
 
     // Temporary hard coded program.
     std::stringstream program;
-    program << "pi 5 pi 6 + pr";
+    program << "si tmp 5 si tmp 6 pi tmp pi 9 + pr";
 
     auto tokens = std::ranges::istream_view<std::string>(program);
     auto it = tokens.begin();
@@ -45,17 +52,19 @@ int main()
         else if (token == OP_PUSH_INT) {
             ++it;
             const auto value = *it;
-            if (is_int(value)) {
-                vs.push(std::stoi(value));
-            } else {
-                fmt::print("Cannot currently load ints from scopes");
-                std::exit(1);
-            }
+            vs.push(fetch_int(value, symbol_table));
         }
         else if (token == OP_ADD) {
             const auto a = vs.pop();
             const auto b = vs.pop();
             vs.push(a + b);
+        }
+        else if (token == OP_STORE_INT) {
+            ++it;
+            const auto name = *it;
+            ++it;
+            const auto value = *it;
+            symbol_table[name] = fetch_int(value, symbol_table);
         }
 
         ++it;
