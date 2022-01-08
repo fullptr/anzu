5db55740dfe4e1bbfb46f0e0472e865470710584
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2022-01-08 20:58:09 +0000

    Change the lex_line function to build tokens by character

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -5,6 +5,31 @@
 #include <sstream>
 
 namespace anzu {
+namespace {
+
+auto lex_line(std::vector<std::string>& tokens, const std::string& line) -> void
+{
+    std::string token;
+    for (auto it = line.begin(); it != line.end(); ++it) {
+        if (*it == '#') { // Ignore comments
+            break;
+        }
+
+        if (!std::isspace(*it)) {
+            token += *it;
+        }
+        else if (!token.empty()) {
+            tokens.push_back(token);
+            token.clear();
+        }
+    }
+
+    if (!token.empty()) {
+        tokens.push_back(token);
+    }
+}
+
+}
 
 auto lex(const std::string& file) -> std::vector<std::string>
 {
@@ -14,12 +39,7 @@ auto lex(const std::string& file) -> std::vector<std::string>
     std::ifstream file_stream{file};
     std::string line;
     while (std::getline(file_stream, line)) {
-        std::istringstream line_stream{line};
-        for (const auto& token : std::ranges::istream_view<std::string>(line_stream)
-                               | std::views::take_while([](auto&& tok) { return tok != "//"; }))
-        {
-            tokens.emplace_back(token);
-        }
+        lex_line(tokens, line);
     }
     return tokens;
 }
