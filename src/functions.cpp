f1a434014b9ed40ab5505438ce50570ed0857282
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2022-01-17 01:16:51 +0000

    Add a print builtin function

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,11 +1,31 @@
 #include "functions.hpp"
+#include "print.hpp"
 
 #include <unordered_map>
 #include <string>
 #include <functional>
 
-static const std::unordered_map<std::string, std::function<void(anzu::context&)>> builtins = {
+namespace {
 
+auto verify(bool condition, std::string_view msg) -> void
+{
+    if (!condition) {
+        anzu::print(msg);
+        std::exit(1);
+    }
+}
+
+auto builtin_print(anzu::context& ctx) -> void
+{
+    verify(!ctx.top().empty(), "frame stack is empty, nothing to print\n");
+    anzu::print("{}", ctx.top().pop());
+    ctx.top().ptr() += 1;
+}
+
+}
+
+static const std::unordered_map<std::string, std::function<void(anzu::context&)>> builtins = {
+    { "print", builtin_print }
 };
 
 namespace anzu {
