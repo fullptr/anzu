61073d60967cd44ea040037d6868c263c1c0cd26
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2022-02-11 01:59:56 +0000

    Cleanup and make function and return keywords

--- src/token.cpp
+++ src/token.cpp
@@ -3,6 +3,14 @@
 
 namespace anzu {
 
+// Returns true if the value is either a keyword or a symbol, which are the only
+// types expected to be consumed by the consume_*, peek, curr and next functions.
+// Other token types are expected to be handled in other ways.
+auto is_consumable_type(token_type type) -> bool
+{
+    return type == token_type::keyword || type == token_type::symbol;
+}
+
 auto to_string(token_type type) -> std::string
 {
     switch (type) {
@@ -32,7 +40,7 @@ tokenstream::tokenstream(const std::vector<token>& tokens)
 
 auto tokenstream::consume_maybe(std::string_view text) -> bool
 {
-    if (curr().text == text && (curr().type == token_type::keyword || curr().type == token_type::symbol)) {
+    if (curr().text == text && is_consumable_type(curr().type)) {
         consume();
         return true;
     }
@@ -45,7 +53,7 @@ auto tokenstream::consume_only(std::string_view text) -> token
         anzu::print("[ERROR] (EOF) expected '{}'\n", text);
         std::exit(1);
     }
-    if (curr().text != text || (curr().type == token_type::name || curr().type == token_type::string)) {
+    if (curr().text != text || !is_consumable_type(curr().type)) {
         const auto [tok_text, line, col, type] = curr();
         anzu::print("[ERROR] ({}:{}) expected '{}', got '{}\n", line, col, text, tok_text);
         std::exit(1);
@@ -55,12 +63,12 @@ auto tokenstream::consume_only(std::string_view text) -> token
 
 auto tokenstream::peek(std::string_view text) -> bool
 {
-    return valid() && curr().text == text && (curr().type == token_type::keyword || curr().type == token_type::symbol);
+    return valid() && curr().text == text && is_consumable_type(curr().type);
 }
 
 auto tokenstream::peek_next(std::string_view text) -> bool
 {
-    return has_next() && next().text == text && (next().type == token_type::keyword || next().type == token_type::symbol);
+    return has_next() && next().text == text  && is_consumable_type(next().type);
 }
 
 }
