2e4b694a3b32be49bc611a40bb979dd03a1fa0f4
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2022-02-19 16:15:23 +0000

    Rename ptr to program_ptr

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -8,41 +8,41 @@ namespace anzu {
 
 auto runtime_context::push_frame() -> frame&
 {
-    d_frames.push_back({});
-    return d_frames.back();
+    frames.push_back({});
+    return frames.back();
 }
 
 auto runtime_context::pop_frame() -> void
 {
-    d_frames.pop_back();
+    frames.pop_back();
 }
 
 auto runtime_context::peek_frame(std::size_t index) -> frame&
 {
-    return d_frames[d_frames.size() - index - 1];
+    return frames[frames.size() - index - 1];
 }
 
 auto runtime_context::push_value(const object& val) -> object&
 {
-    d_values.push_back(val);
-    return d_values.back();
+    stack.push_back(val);
+    return stack.back();
 }
 
 auto runtime_context::pop_value() -> object
 {
-    auto ret = d_values.back();
-    d_values.pop_back();
+    auto ret = stack.back();
+    stack.pop_back();
     return ret;
 }
 
 auto runtime_context::peek_value(std::size_t index) -> object&
 {
-    return d_values[d_values.size() - index - 1];
+    return stack[stack.size() - index - 1];
 }
 
 auto runtime_context::size() const -> std::size_t
 {
-    return d_values.size();
+    return stack.size();
 }
 
 auto apply_op(runtime_context& ctx, const op& op_code) -> void
@@ -50,104 +50,104 @@ auto apply_op(runtime_context& ctx, const op& op_code) -> void
     std::visit(overloaded {
         [&](const op_load_literal& op) {
             ctx.push_value(op.value);
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_load_local& op) {
             auto& frame = ctx.peek_frame();
             const auto idx = frame.base_ptr + op.offset;
-            ctx.push_value(ctx.values[idx]);
-            frame.ptr += 1;
+            ctx.push_value(ctx.memory[idx]);
+            frame.program_ptr += 1;
         },
         [&](const op_load_global& op) {
             auto& frame = ctx.peek_frame();
             const auto idx = op.position;
-            ctx.push_value(ctx.values[idx]);
-            frame.ptr += 1;
+            ctx.push_value(ctx.memory[idx]);
+            frame.program_ptr += 1;
         },
         [&](const op_pop& op) {
             ctx.pop_value();
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_copy_index& op) {
             ctx.push_value(ctx.peek_value(op.index));
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_save_local& op) {
             auto& frame = ctx.peek_frame();
             const auto idx = frame.base_ptr + op.offset;
-            if (idx >= ctx.values.size()) {
-                ctx.values.resize(idx + 1);
+            if (idx >= ctx.memory.size()) {
+                ctx.memory.resize(idx + 1);
             }
-            ctx.values[idx] = ctx.pop_value();
-            frame.ptr += 1;
+            ctx.memory[idx] = ctx.pop_value();
+            frame.program_ptr += 1;
         },
         [&](const op_save_global& op) {
             auto& frame = ctx.peek_frame();
             const auto idx = op.position;
-            if (idx >= ctx.values.size()) {
-                ctx.values.resize(idx + 1);
+            if (idx >= ctx.memory.size()) {
+                ctx.memory.resize(idx + 1);
             }
-            ctx.values[idx] = ctx.pop_value();
-            frame.ptr += 1;
+            ctx.memory[idx] = ctx.pop_value();
+            frame.program_ptr += 1;
         },
         [&](const op_if& op) {
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_if_end& op) {
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_else& op) {
-            ctx.peek_frame().ptr = op.jump;
+            ctx.peek_frame().program_ptr = op.jump;
         },
         [&](const op_while& op) {
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_while_end& op) {
-            ctx.peek_frame().ptr = op.jump;
+            ctx.peek_frame().program_ptr = op.jump;
         },
         [&](const op_for& op) {
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_for_end& op) {
-            ctx.peek_frame().ptr = op.jump;
+            ctx.peek_frame().program_ptr = op.jump;
         },
         [&](const op_break& op) {
-            ctx.peek_frame().ptr = op.jump;
+            ctx.peek_frame().program_ptr = op.jump;
         },
         [&](const op_continue& op) {
-            ctx.peek_frame().ptr = op.jump;
+            ctx.peek_frame().program_ptr = op.jump;
         },
         [&](const op_jump_if_false& op) {
             if (ctx.pop_value().to_bool()) {
-                ctx.peek_frame().ptr += 1;
+                ctx.peek_frame().program_ptr += 1;
             } else {
-                ctx.peek_frame().ptr = op.jump;
+                ctx.peek_frame().program_ptr = op.jump;
             }
         },
         [&](const op_function& op) {
-            ctx.peek_frame().ptr = op.jump;
+            ctx.peek_frame().program_ptr = op.jump;
         },
         [&](const op_function_end& op) {
-            const auto num_to_pop = ctx.values.size() - ctx.peek_frame().base_ptr;
+            const auto num_to_pop = ctx.memory.size() - ctx.peek_frame().base_ptr;
             for (std::size_t i = 0; i != num_to_pop; ++i) {
-                ctx.values.pop_back();
+                ctx.memory.pop_back();
             }
             ctx.push_value(anzu::null_object());
             ctx.pop_frame();
         },
         [&](const op_return& op) {
-            const auto num_to_pop = ctx.values.size() - ctx.peek_frame().base_ptr;
+            const auto num_to_pop = ctx.memory.size() - ctx.peek_frame().base_ptr;
             for (std::size_t i = 0; i != num_to_pop; ++i) {
-                ctx.values.pop_back();
+                ctx.memory.pop_back();
             }
             ctx.pop_frame();
         },
         [&](const op_function_call& op) {
-            ctx.peek_frame().ptr += 1; // Position after function call
+            ctx.peek_frame().program_ptr += 1; // Position after function call
 
             auto& frame = ctx.push_frame();
-            frame.base_ptr = ctx.values.size();
-            frame.ptr = op.ptr; // Jump into the function
+            frame.base_ptr = ctx.memory.size();
+            frame.program_ptr = op.ptr; // Jump into the function
         },
         [&](const op_builtin_call& op) {
             const auto argc = op.sig.args.size();
@@ -159,85 +159,85 @@ auto apply_op(runtime_context& ctx, const op& op_code) -> void
 
             // Call the builtin function with the given args and push the return value
             ctx.push_value(op.ptr(args));
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_add& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(a + b);
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_sub& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(a - b);
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_mul& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(a * b);
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_div& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(a / b);
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_mod& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(a % b);
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_eq& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(object{a == b});
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_ne& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(object{a != b});
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_lt& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(object{a < b});
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_le& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(object{a <= b});
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_gt& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(object{a > b});
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_ge& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(object{a >= b});
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_or& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(object{a || b});
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_and& op) {
             auto b = ctx.pop_value();
             auto a = ctx.pop_value();
             ctx.push_value(object{a && b});
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_build_list& op) {
             auto list = std::make_shared<std::vector<anzu::object>>();
@@ -245,11 +245,11 @@ auto apply_op(runtime_context& ctx, const op& op_code) -> void
                 list->push_back(ctx.pop_value());
             }
             ctx.push_value(object{list});
-            ctx.peek_frame().ptr += 1;
+            ctx.peek_frame().program_ptr += 1;
         },
         [&](const op_debug& op) {
             auto& frame = ctx.peek_frame();
-            frame.ptr += 1;
+            frame.program_ptr += 1;
         }
     }, op_code);
 }
@@ -259,21 +259,21 @@ auto run_program(const anzu::program& program) -> void
     runtime_context ctx;
     ctx.push_frame();
 
-    while (ctx.peek_frame().ptr < std::ssize(program)) {
-        apply_op(ctx, program[ctx.peek_frame().ptr]);
+    while (ctx.peek_frame().program_ptr < std::ssize(program)) {
+        apply_op(ctx, program[ctx.peek_frame().program_ptr]);
     }
 }
 
 auto run_program_debug(const anzu::program& program) -> void
 {
     anzu::runtime_context ctx;
-    ctx.values.reserve(1000);
+    ctx.memory.reserve(1000);
     ctx.push_frame();
 
-    while (ctx.peek_frame().ptr < std::ssize(program)) {
-        const auto& op = program[ctx.peek_frame().ptr];
-        anzu::print("{:>4} - {}\n", ctx.peek_frame().ptr, anzu::to_string(op));
-        apply_op(ctx, program[ctx.peek_frame().ptr]);
+    while (ctx.peek_frame().program_ptr < std::ssize(program)) {
+        const auto& op = program[ctx.peek_frame().program_ptr];
+        anzu::print("{:>4} - {}\n", ctx.peek_frame().program_ptr, anzu::to_string(op));
+        apply_op(ctx, program[ctx.peek_frame().program_ptr]);
     }
 }
 
--- src/runtime.hpp
+++ src/runtime.hpp
@@ -2,25 +2,18 @@
 #include "object.hpp"
 #include "program.hpp"
 
-#include <stack>
-#include <string>
-#include <variant>
-#include <ranges>
+#include <vector>
 
 namespace anzu {
 
 struct frame
 {
-    std::intptr_t ptr = 0;
+    std::intptr_t program_ptr = 0;
     std::intptr_t base_ptr = 0;
 };
 
-class runtime_context
+struct runtime_context
 {
-    std::vector<frame>        d_frames;
-    std::vector<anzu::object> d_values;
-
-public:
     auto push_frame() -> frame&;
     auto pop_frame() -> void;
     auto peek_frame(std::size_t index = 0) -> frame&;
@@ -30,7 +23,9 @@ public:
     auto peek_value(std::size_t index = 0) -> object&;
     auto size() const -> std::size_t;
 
-    std::vector<anzu::object> values;
+    std::vector<frame>        frames;
+    std::vector<anzu::object> stack;
+    std::vector<anzu::object> memory;
 };
 
 auto run_program(const anzu::program& program) -> void;
