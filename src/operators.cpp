0cb932db7ae64092c9929804c14ffd120f1fe495
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2022-03-16 03:54:04 +0000

    Remove repetition of types

--- src/operators.cpp
+++ src/operators.cpp
@@ -11,12 +11,12 @@ auto get_back(std::vector<block>& mem, std::size_t index) -> T&
     return std::get<std::remove_cvref_t<T>>(mem[mem.size() - index - 1]);
 }
 
-template <typename Lhs, typename Op, typename Rhs>
+template <typename Type, template <class> class Op>
 auto bin_op(std::vector<block>& mem)
 {
-    const auto op = Op{};
-    const auto rhs = get_back<Lhs>(mem, 0);
-    const auto lhs = get_back<Rhs>(mem, 1);
+    const auto op = Op<Type>{};
+    const auto rhs = get_back<Type>(mem, 0);
+    const auto lhs = get_back<Type>(mem, 1);
     mem.pop_back();
     mem.back().emplace<decltype(op(lhs, rhs))>(op(lhs, rhs));
 }
@@ -36,47 +36,47 @@ auto resolve_bin_op(const bin_op_description& desc) -> std::optional<bin_op_info
 
     if (type == int_type()) {
         if (desc.op == tk_add) {
-            return bin_op_info{ bin_op<block_int, std::plus<block_int>, block_int>, type };
+            return bin_op_info{ bin_op<block_int, std::plus>, type };
         } else if (desc.op == tk_sub) {
-            return bin_op_info{ bin_op<block_int, std::minus<block_int>, block_int>, type };
+            return bin_op_info{ bin_op<block_int, std::minus>, type };
         } else if (desc.op == tk_mul) {
-            return bin_op_info{ bin_op<block_int, std::multiplies<block_int>, block_int>, type };
+            return bin_op_info{ bin_op<block_int, std::multiplies>, type };
         } else if (desc.op == tk_div) {
-            return bin_op_info{ bin_op<block_int, std::divides<block_int>, block_int>, type };
+            return bin_op_info{ bin_op<block_int, std::divides>, type };
         } else if (desc.op == tk_mod) {
-            return bin_op_info{ bin_op<block_int, std::modulus<block_int>, block_int>, type };
+            return bin_op_info{ bin_op<block_int, std::modulus>, type };
         } else if (desc.op == "<") {
-            return bin_op_info{ bin_op<block_int, std::less<block_int>, block_int>, bool_type() };
+            return bin_op_info{ bin_op<block_int, std::less>, bool_type() };
         } else if (desc.op == "<=") {
-            return bin_op_info{ bin_op<block_int, std::less_equal<block_int>, block_int>, bool_type() };
+            return bin_op_info{ bin_op<block_int, std::less_equal>, bool_type() };
         } else if (desc.op == ">") {
-            return bin_op_info{ bin_op<block_int, std::greater<block_int>, block_int>, bool_type() };
+            return bin_op_info{ bin_op<block_int, std::greater>, bool_type() };
         } else if (desc.op == ">=") {
-            return bin_op_info{ bin_op<block_int, std::greater_equal<block_int>, block_int>, bool_type() };
+            return bin_op_info{ bin_op<block_int, std::greater_equal>, bool_type() };
         } else if (desc.op == "==") {
-            return bin_op_info{ bin_op<block_int, std::equal_to<block_int>, block_int>, bool_type() };
+            return bin_op_info{ bin_op<block_int, std::equal_to>, bool_type() };
         } else if (desc.op == "!=") {
-            return bin_op_info{ bin_op<block_int, std::not_equal_to<block_int>, block_int>, bool_type() };
+            return bin_op_info{ bin_op<block_int, std::not_equal_to>, bool_type() };
         }
     }
     else if (type == bool_type()) {
         if (desc.op == "==") {
-            return bin_op_info{ bin_op<block_bool, std::equal_to<block_bool>, block_bool>, type };
+            return bin_op_info{ bin_op<block_bool, std::equal_to>, type };
         } else if (desc.op == "!=") {
-            return bin_op_info{ bin_op<block_bool, std::not_equal_to<block_bool>, block_bool>, type };
+            return bin_op_info{ bin_op<block_bool, std::not_equal_to>, type };
         } else if (desc.op == "&&") {
-            return bin_op_info{ bin_op<block_bool, std::logical_and<block_bool>, block_bool>, type };
+            return bin_op_info{ bin_op<block_bool, std::logical_and>, type };
         } else if (desc.op == "||") {
-            return bin_op_info{ bin_op<block_bool, std::logical_or<block_bool>, block_bool>, type };
+            return bin_op_info{ bin_op<block_bool, std::logical_or>, type };
         }
     }
     else if (type == str_type()) {
         if (desc.op == "+") {
-            return bin_op_info{ bin_op<block_str, std::plus<block_str>, block_str>, type };
+            return bin_op_info{ bin_op<block_str, std::plus>, type };
         } else if (desc.op == "==") {
-            return bin_op_info{ bin_op<block_str, std::equal_to<block_str>, block_str>, bool_type() };
+            return bin_op_info{ bin_op<block_str, std::equal_to>, bool_type() };
         } else if (desc.op == "!=") {
-            return bin_op_info{ bin_op<block_str, std::not_equal_to<block_str>, block_str>, bool_type() };
+            return bin_op_info{ bin_op<block_str, std::not_equal_to>, bool_type() };
         }
     }
 
