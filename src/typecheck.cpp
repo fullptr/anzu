531a6d9789aed6405570a897a1d5b00d26eee534
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2022-03-18 18:16:57 +0000

    Implement typechecking for field assignment

--- src/typecheck.cpp
+++ src/typecheck.cpp
@@ -425,6 +425,48 @@ auto typecheck_node(typecheck_context& ctx, const node_assignment_stmt& node) ->
 
 auto typecheck_node(typecheck_context& ctx, const node_field_assignment_stmt& node) -> void
 {
+    if (ctx.locals) {
+        if (auto it = ctx.locals->find(node.name); it != ctx.locals->end()) {
+            auto type = it->second;
+            for (const auto& field_name : node.fields) {
+                for (const auto& field : ctx.registered_types.get_fields(type)) {
+                    if (field.name == field_name) {
+                        type = field.type;
+                        break;
+                    }
+                    type_error(node.token, "type '{}' has no field '{}\n", type, field_name);
+                }
+            }
+
+            const auto rhs_type = typecheck_expr(ctx, *node.expr);
+            if (rhs_type != type) {
+                type_error(
+                    node.token, "(field assignment) tried to assign '{}' to '{}'\n", rhs_type, type
+                );
+            }
+            return;
+        }
+    }
+    if (auto it = ctx.globals.find(node.name); it != ctx.globals.end()) {
+        auto type = it->second;
+        for (const auto& field_name : node.fields) {
+            for (const auto& field : ctx.registered_types.get_fields(type)) {
+                if (field.name == field_name) {
+                    type = field.type;
+                    break;
+                }
+                type_error(node.token, "type '{}' has no field '{}\n", type, field_name);
+            }
+        }
+
+        const auto rhs_type = typecheck_expr(ctx, *node.expr);
+        if (rhs_type != type) {
+            type_error(
+                node.token, "(field assignment) tried to assign '{}' to '{}'\n", rhs_type, type
+            );
+        }
+        return;
+    }
     print("typechecking for node field assignment not implemented\n");
 }
 
