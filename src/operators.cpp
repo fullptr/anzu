e7029165d910db4267a3d6abb45d137c34a8406b
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2022-04-15 17:31:36 +0100

    replace bool_negate with logical_not

--- src/operators.cpp
+++ src/operators.cpp
@@ -7,93 +7,57 @@
 namespace anzu {
 namespace {
 
-template <typename T>
-auto get_back(std::vector<std::byte>& mem, std::size_t index) -> T&
-{
-    return std::get<std::remove_cvref_t<T>>(mem[mem.size() - index - 1]);
-}
-
 auto pop_n(std::vector<std::byte>& mem, std::size_t n) -> void
 {
     mem.resize(mem.size() - n);
 }
 
+// TODO: Dedeup this, also appears in runtime
+template <typename T>
+auto push_value(std::vector<std::byte>& mem, const T& value) -> void
+{
+    for (const auto& b : std::bit_cast<std::array<std::byte, sizeof(T)>>(value)) {
+        mem.push_back(b);
+    }
+}
+
+// TODO: Dedeup this, also appears in runtime
+template <typename T>
+auto pop_value(std::vector<std::byte>& mem) -> T
+{
+    auto ret = T{};
+    std::memcpy(&ret, &mem[mem.size() - sizeof(T)], sizeof(T));
+    pop_n(mem, sizeof(T));
+    return ret;
+}
+
 template <typename Type, template <typename> typename Op>
 auto bin_op_bytes(std::vector<std::byte>& mem) -> void
 {
-    const auto op = Op<Type>{};
-    auto lhs_bytes = std::vector<std::byte>{};
-    for (std::size_t i = 0; i != sizeof(Type); ++i) {
-        lhs_bytes.push_back(mem[mem.size() - (2 * sizeof(Type)) + i]);
-    }
-    const auto lhs = from_bytes<Type>(lhs_bytes);
-    auto rhs_bytes = std::vector<std::byte>{};
-    for (std::size_t i = 0; i != sizeof(Type); ++i) {
-        rhs_bytes.push_back(mem[mem.size() - sizeof(Type) + i]);
-    }
-    const auto rhs = from_bytes<Type>(rhs_bytes);
-    pop_n(mem, 2 * sizeof(Type));
-    const auto ret = op(lhs, rhs);
-    const auto ret_bytes = std::bit_cast<std::array<std::byte, sizeof(ret)>>(ret);
-    for (const auto& b : ret_bytes) {
-        mem.push_back(b);
-    }
-}
-
-// TODO: Dedeup this, also appears in runtime
-auto push_u64(std::vector<std::byte>& mem, std::uint64_t value) -> void
-{
-    for (const auto& b : std::bit_cast<std::array<std::byte, sizeof(std::uint64_t)>>(value)) {
-        mem.push_back(b);
-    }
-}
-
-// TODO: Dedeup this, also appears in runtime
-auto pop_u64(std::vector<std::byte>& mem) -> std::uint64_t
-{
-    auto bytes = std::array<std::byte, sizeof(std::uint64_t)>{};
-    for (std::size_t i = 0; i != sizeof(std::uint64_t); ++i) {
-        bytes[i] = mem[mem.size() - sizeof(std::uint64_t) + i];
-    }
-    for (std::size_t i = 0; i != sizeof(std::uint64_t); ++i) {
-        mem.pop_back();
-    }
-    return std::bit_cast<std::uint64_t>(bytes);
+    static constexpr auto op = Op<Type>{};
+    const auto rhs = pop_value<Type>(mem);
+    const auto lhs = pop_value<Type>(mem);
+    push_value(mem, op(lhs, rhs));
 }
 
 // Top of stack: [ptr], [size], [offset]
 // offset is popped, size stays the same, ptr is modified
 auto ptr_addition(std::vector<std::byte>& mem)
 {
-    const auto offset = pop_u64(mem);
-    const auto size = pop_u64(mem);
-    const auto ptr = pop_u64(mem);
+    const auto offset = pop_value<std::uint64_t>(mem);
+    const auto size = pop_value<std::uint64_t>(mem);
+    const auto ptr = pop_value<std::uint64_t>(mem);
 
-    push_u64(mem, ptr + offset * size);
-    push_u64(mem, size);
+    push_value(mem, ptr + offset * size);
+    push_value(mem, size);
 }
 
 template <typename Type, template <typename> typename Op>
 auto unary_op_sized(std::vector<std::byte>& mem)
 {
-    const auto op = Op<Type>{};
-    auto obj_bytes = std::vector<std::byte>{};
-    for (std::size_t i = 0; i != sizeof(Type); ++i) {
-        obj_bytes.push_back(mem[mem.size() - (2 * sizeof(Type)) + i]);
-    }
-    const auto obj = from_bytes<Type>(obj_bytes);
-    pop_n(mem, sizeof(Type));
-    const auto ret = op(obj);
-    const auto ret_bytes = std::bit_cast<std::array<std::byte, sizeof(ret)>>(ret);
-    for (const auto& b : ret_bytes) {
-        mem.push_back(b);
-    }
-}
-
-auto bool_negate(std::vector<std::byte>& mem)
-{
-    auto& top = mem.back();
-    top = (top == std::byte{1}) ? std::byte{0} : std::byte{1};
+    static constexpr auto op = Op<Type>{};
+    const auto obj = pop_value<Type>(mem);
+    push_value(mem, op(obj));
 }
 
 }
@@ -254,7 +218,7 @@ auto resolve_unary_op(const unary_op_description& desc) -> std::optional<unary_o
     }
     else if (type == bool_type()) {
         if (desc.op == tk_bang) {
-            return unary_op_info{ bool_negate, type };
+            return unary_op_info{ unary_op_sized<bool, std::logical_not>, type };
         }
     }
     return std::nullopt;
