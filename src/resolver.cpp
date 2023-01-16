2190e0494c62d4b7f85c19000add4500882a4a9d
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2023-01-16 01:37:01 +0000

    Add the new ops to the resolver

--- src/resolver.cpp
+++ src/resolver.cpp
@@ -16,24 +16,52 @@ auto resolve_operation(
         if (operation == "-") return op_info{ {op_i32_sub{}}, type };
         if (operation == "*") return op_info{ {op_i32_mul{}}, type };
         if (operation == "/") return op_info{ {op_i32_div{}}, type };
+
+        if (operation == "==") return op_info{ {op_i32_eq{}}, bool_type() };
+        if (operation == "!=") return op_info{ {op_i32_ne{}}, bool_type() };
+        if (operation == "<")  return op_info{ {op_i32_lt{}}, bool_type() };
+        if (operation == "<=") return op_info{ {op_i32_le{}}, bool_type() };
+        if (operation == ">")  return op_info{ {op_i32_gt{}}, bool_type() };
+        if (operation == ">=") return op_info{ {op_i32_ge{}}, bool_type() };
     }
     else if (type == i64_type()) {
         if (operation == "+") return op_info{ {op_i64_add{}}, type };
         if (operation == "-") return op_info{ {op_i64_sub{}}, type };
         if (operation == "*") return op_info{ {op_i64_mul{}}, type };
         if (operation == "/") return op_info{ {op_i64_div{}}, type };
+
+        if (operation == "==") return op_info{ {op_i64_eq{}}, bool_type() };
+        if (operation == "!=") return op_info{ {op_i64_ne{}}, bool_type() };
+        if (operation == "<")  return op_info{ {op_i64_lt{}}, bool_type() };
+        if (operation == "<=") return op_info{ {op_i64_le{}}, bool_type() };
+        if (operation == ">")  return op_info{ {op_i64_gt{}}, bool_type() };
+        if (operation == ">=") return op_info{ {op_i64_ge{}}, bool_type() };
     }
     else if (type == u64_type()) {
         if (operation == "+") return op_info{ {op_u64_add{}}, type };
         if (operation == "-") return op_info{ {op_u64_sub{}}, type };
         if (operation == "*") return op_info{ {op_u64_mul{}}, type };
         if (operation == "/") return op_info{ {op_u64_div{}}, type };
+
+        if (operation == "==") return op_info{ {op_u64_eq{}}, bool_type() };
+        if (operation == "!=") return op_info{ {op_u64_ne{}}, bool_type() };
+        if (operation == "<")  return op_info{ {op_u64_lt{}}, bool_type() };
+        if (operation == "<=") return op_info{ {op_u64_le{}}, bool_type() };
+        if (operation == ">")  return op_info{ {op_u64_gt{}}, bool_type() };
+        if (operation == ">=") return op_info{ {op_u64_ge{}}, bool_type() };
     }
     else if (type == f64_type()) {
         if (operation == "+") return op_info{ {op_f64_add{}}, type };
         if (operation == "-") return op_info{ {op_f64_sub{}}, type };
         if (operation == "*") return op_info{ {op_f64_mul{}}, type };
         if (operation == "/") return op_info{ {op_f64_div{}}, type };
+
+        if (operation == "==") return op_info{ {op_f64_eq{}}, bool_type() };
+        if (operation == "!=") return op_info{ {op_f64_ne{}}, bool_type() };
+        if (operation == "<")  return op_info{ {op_f64_lt{}}, bool_type() };
+        if (operation == "<=") return op_info{ {op_f64_le{}}, bool_type() };
+        if (operation == ">")  return op_info{ {op_f64_gt{}}, bool_type() };
+        if (operation == ">=") return op_info{ {op_f64_ge{}}, bool_type() };
     }
     else if (type == bool_type()) {
         if (operation == "&&") return op_info{ {op_bool_and{}}, type };
