855d4e819e6c6ab3d9a5e3147ddb559ce7e4f26c
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2023-01-16 01:40:18 +0000

    Use vocab constants

--- src/resolver.cpp
+++ src/resolver.cpp
@@ -1,5 +1,6 @@
 #include "resolver.hpp"
 #include "program.hpp"
+#include "vocabulary.hpp"
 
 namespace anzu {
 
@@ -12,62 +13,62 @@ auto resolve_operation(
 
     const auto& type = lhs;
     if (type == i32_type()) {
-        if (operation == "+") return op_info{ {op_i32_add{}}, type };
-        if (operation == "-") return op_info{ {op_i32_sub{}}, type };
-        if (operation == "*") return op_info{ {op_i32_mul{}}, type };
-        if (operation == "/") return op_info{ {op_i32_div{}}, type };
+        if (operation == tk_add) return op_info{ {op_i32_add{}}, type };
+        if (operation == tk_sub) return op_info{ {op_i32_sub{}}, type };
+        if (operation == tk_mul) return op_info{ {op_i32_mul{}}, type };
+        if (operation == tk_div) return op_info{ {op_i32_div{}}, type };
 
-        if (operation == "==") return op_info{ {op_i32_eq{}}, bool_type() };
-        if (operation == "!=") return op_info{ {op_i32_ne{}}, bool_type() };
-        if (operation == "<")  return op_info{ {op_i32_lt{}}, bool_type() };
-        if (operation == "<=") return op_info{ {op_i32_le{}}, bool_type() };
-        if (operation == ">")  return op_info{ {op_i32_gt{}}, bool_type() };
-        if (operation == ">=") return op_info{ {op_i32_ge{}}, bool_type() };
+        if (operation == tk_eq) return op_info{ {op_i32_eq{}}, bool_type() };
+        if (operation == tk_ne) return op_info{ {op_i32_ne{}}, bool_type() };
+        if (operation == tk_lt) return op_info{ {op_i32_lt{}}, bool_type() };
+        if (operation == tk_le) return op_info{ {op_i32_le{}}, bool_type() };
+        if (operation == tk_gt) return op_info{ {op_i32_gt{}}, bool_type() };
+        if (operation == tk_ge) return op_info{ {op_i32_ge{}}, bool_type() };
     }
     else if (type == i64_type()) {
-        if (operation == "+") return op_info{ {op_i64_add{}}, type };
-        if (operation == "-") return op_info{ {op_i64_sub{}}, type };
-        if (operation == "*") return op_info{ {op_i64_mul{}}, type };
-        if (operation == "/") return op_info{ {op_i64_div{}}, type };
+        if (operation == tk_add) return op_info{ {op_i64_add{}}, type };
+        if (operation == tk_sub) return op_info{ {op_i64_sub{}}, type };
+        if (operation == tk_mul) return op_info{ {op_i64_mul{}}, type };
+        if (operation == tk_div) return op_info{ {op_i64_div{}}, type };
 
-        if (operation == "==") return op_info{ {op_i64_eq{}}, bool_type() };
-        if (operation == "!=") return op_info{ {op_i64_ne{}}, bool_type() };
-        if (operation == "<")  return op_info{ {op_i64_lt{}}, bool_type() };
-        if (operation == "<=") return op_info{ {op_i64_le{}}, bool_type() };
-        if (operation == ">")  return op_info{ {op_i64_gt{}}, bool_type() };
-        if (operation == ">=") return op_info{ {op_i64_ge{}}, bool_type() };
+        if (operation == tk_eq) return op_info{ {op_i64_eq{}}, bool_type() };
+        if (operation == tk_ne) return op_info{ {op_i64_ne{}}, bool_type() };
+        if (operation == tk_lt) return op_info{ {op_i64_lt{}}, bool_type() };
+        if (operation == tk_le) return op_info{ {op_i64_le{}}, bool_type() };
+        if (operation == tk_gt) return op_info{ {op_i64_gt{}}, bool_type() };
+        if (operation == tk_ge) return op_info{ {op_i64_ge{}}, bool_type() };
     }
     else if (type == u64_type()) {
-        if (operation == "+") return op_info{ {op_u64_add{}}, type };
-        if (operation == "-") return op_info{ {op_u64_sub{}}, type };
-        if (operation == "*") return op_info{ {op_u64_mul{}}, type };
-        if (operation == "/") return op_info{ {op_u64_div{}}, type };
+        if (operation == tk_add) return op_info{ {op_u64_add{}}, type };
+        if (operation == tk_sub) return op_info{ {op_u64_sub{}}, type };
+        if (operation == tk_mul) return op_info{ {op_u64_mul{}}, type };
+        if (operation == tk_div) return op_info{ {op_u64_div{}}, type };
 
-        if (operation == "==") return op_info{ {op_u64_eq{}}, bool_type() };
-        if (operation == "!=") return op_info{ {op_u64_ne{}}, bool_type() };
-        if (operation == "<")  return op_info{ {op_u64_lt{}}, bool_type() };
-        if (operation == "<=") return op_info{ {op_u64_le{}}, bool_type() };
-        if (operation == ">")  return op_info{ {op_u64_gt{}}, bool_type() };
-        if (operation == ">=") return op_info{ {op_u64_ge{}}, bool_type() };
+        if (operation == tk_eq) return op_info{ {op_u64_eq{}}, bool_type() };
+        if (operation == tk_ne) return op_info{ {op_u64_ne{}}, bool_type() };
+        if (operation == tk_lt) return op_info{ {op_u64_lt{}}, bool_type() };
+        if (operation == tk_le) return op_info{ {op_u64_le{}}, bool_type() };
+        if (operation == tk_gt) return op_info{ {op_u64_gt{}}, bool_type() };
+        if (operation == tk_ge) return op_info{ {op_u64_ge{}}, bool_type() };
     }
     else if (type == f64_type()) {
-        if (operation == "+") return op_info{ {op_f64_add{}}, type };
-        if (operation == "-") return op_info{ {op_f64_sub{}}, type };
-        if (operation == "*") return op_info{ {op_f64_mul{}}, type };
-        if (operation == "/") return op_info{ {op_f64_div{}}, type };
+        if (operation == tk_add) return op_info{ {op_f64_add{}}, type };
+        if (operation == tk_sub) return op_info{ {op_f64_sub{}}, type };
+        if (operation == tk_mul) return op_info{ {op_f64_mul{}}, type };
+        if (operation == tk_div) return op_info{ {op_f64_div{}}, type };
 
-        if (operation == "==") return op_info{ {op_f64_eq{}}, bool_type() };
-        if (operation == "!=") return op_info{ {op_f64_ne{}}, bool_type() };
-        if (operation == "<")  return op_info{ {op_f64_lt{}}, bool_type() };
-        if (operation == "<=") return op_info{ {op_f64_le{}}, bool_type() };
-        if (operation == ">")  return op_info{ {op_f64_gt{}}, bool_type() };
-        if (operation == ">=") return op_info{ {op_f64_ge{}}, bool_type() };
+        if (operation == tk_eq) return op_info{ {op_f64_eq{}}, bool_type() };
+        if (operation == tk_ne) return op_info{ {op_f64_ne{}}, bool_type() };
+        if (operation == tk_lt) return op_info{ {op_f64_lt{}}, bool_type() };
+        if (operation == tk_le) return op_info{ {op_f64_le{}}, bool_type() };
+        if (operation == tk_gt) return op_info{ {op_f64_gt{}}, bool_type() };
+        if (operation == tk_ge) return op_info{ {op_f64_ge{}}, bool_type() };
     }
     else if (type == bool_type()) {
-        if (operation == "&&") return op_info{ {op_bool_and{}}, type };
-        if (operation == "||") return op_info{ {op_bool_or{}}, type };
-        if (operation == "==") return op_info{ {op_bool_eq{}}, type };
-        if (operation == "!=") return op_info{ {op_bool_ne{}}, type };
+        if (operation == tk_and) return op_info{ {op_bool_and{}}, type };
+        if (operation == tk_or) return op_info{ {op_bool_or{}}, type };
+        if (operation == tk_eq) return op_info{ {op_bool_eq{}}, type };
+        if (operation == tk_ne) return op_info{ {op_bool_ne{}}, type };
     }
     return std::nullopt;
 }
