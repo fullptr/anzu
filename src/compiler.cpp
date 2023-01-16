9a8e52121ce76202cf2c54b4a4948d585713ef6f
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2023-01-16 01:26:33 +0000

    Prefer the resolver over the old way

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -547,18 +547,17 @@ auto type_of_expr(const compiler& com, const node_expr& node) -> type_name
         [&](const node_binary_op_expr& expr) {
             const auto lhs = type_of_expr(com, *expr.lhs);
             const auto rhs = type_of_expr(com, *expr.rhs);
-            const auto desc = binary_op_description{
-                .op = expr.token.text, .lhs=lhs, .rhs=rhs
-            };
-            const auto info1 = resolve_binary_op(com.types, desc);
-            const auto info2 = resolve_operation(lhs, rhs, expr.token.text);
-            if (info1.has_value()) {
-                return info1->result_type;
+            const auto op = expr.token.text;
+
+            const auto desc = binary_op_description{ .op = op, .lhs = lhs, .rhs = rhs };
+            
+            if (const auto info = resolve_operation(lhs, rhs, op)) {
+                return info->return_type;
             }
-            else if (info2.has_value()) {
-                return info2->return_type;
+            else if (const auto info = resolve_binary_op(com.types, desc)) {
+                return info->result_type;
             }
-            expr.token.error("could not find op '{} {} {}'", desc.lhs, desc.op, desc.rhs);
+            expr.token.error("could not find op '{} {} {}'", lhs, op, desc.rhs);
             return null_type();
         },
         [&](const node_function_call_expr& expr) {
@@ -697,21 +696,21 @@ auto push_expr_val(compiler& com, const node_binary_op_expr& node) -> type_name
     const auto rhs = push_expr_val(com, *node.rhs);
     const auto op = node.token.text;
 
-    const auto info1 = resolve_binary_op(com.types, { .op=op, .lhs=lhs, .rhs=rhs });
-    const auto info2 = resolve_operation(lhs, rhs, op);
-    if (info1.has_value()) {
+    const auto desc = binary_op_description{ .op = op, .lhs = lhs, .rhs = rhs };
+    
+    if (const auto info = resolve_operation(lhs, rhs, op)) {
+        com.program.emplace_back(info->op_code);
+        return info->return_type;
+    }
+    else if (const auto info = resolve_binary_op(com.types, desc)) {
         com.program.emplace_back(op_builtin_call{
             .name = std::format("{} {} {}", lhs, op, rhs),
-            .ptr = info1->operator_func
+            .ptr = info->operator_func
         });
-        return info1->result_type;
-    }
-    else if (info2.has_value()) {
-        com.program.emplace_back(info2->op_code);
-        return info2->return_type;
+        return info->result_type;
     }
 
-    node.token.error("could not evaluate '{} {} {}'", lhs, op, rhs);
+    node.token.error("could not find op '{} {} {}'", lhs, op, rhs);
     return null_type();
 }
 
