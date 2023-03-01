500ef25d13d3e7aba03a861340a3395f6e134b3b
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2023-03-01 18:18:53 +0000

    Replace the old token with the new lex_token type

--- src/token.hpp
+++ src/token.hpp
@@ -8,25 +8,79 @@
 
 namespace anzu {
 
-enum class token_type
+enum class lex_token_type
 {
-    keyword,
-    symbol,
-    name,
-    character,
-    i32,
-    i64,
-    u64,
-    f64,
-    string
+    eof,
+    placeholder,
+    number,
+    identifier,
+    string,
+    kw_assert,
+    kw_bool,
+    kw_break,
+    kw_char,
+    kw_continue,
+    kw_default,
+    kw_delete,
+    kw_else,
+    kw_f64,
+    kw_false,
+    kw_for,
+    kw_function,
+    kw_i32,
+    kw_i64,
+    kw_if,
+    kw_import,
+    kw_in,
+    kw_loop,
+    kw_new,
+    kw_null,
+    kw_return,
+    kw_sizeof,
+    kw_struct,
+    kw_true,
+    kw_typeof,
+    kw_u64,
+    kw_while,
+    left_paren,
+    right_paren,
+    left_brace,
+    right_brace,
+    semicolon,
+    comma,
+    dot,
+    minus,
+    plus,
+    slash,
+    star,
+    bang_equal,
+    bang,
+    equal_equal,
+    equal,
+    less_equal,
+    less,
+    greater_equal,
+    greater,
+    ampersand,
+    ampersand_ampersand,
+    colon_equal,
+    colon,
+    left_bracket,
+    right_bracket,
+    percent,
+    bar_bar,
+    bar,
+    arrow,
 };
 
-struct token
+auto to_string(lex_token_type tt) -> std::string_view;
+
+struct lex_token
 {
-    std::string  text;
-    std::int64_t line;
-    std::int64_t col;
-    token_type   type;
+    std::string_view text;
+    std::size_t      line;
+    std::size_t      col;
+    lex_token_type   type;
 
     [[noreturn]] void error(std::string_view msg) const;
 
@@ -52,15 +106,14 @@ struct token
     }
 };
 
-auto to_string(token_type type) -> std::string;
-auto print_tokens(const std::vector<anzu::token>& tokens) -> void;
+auto print_tokens(const std::vector<anzu::lex_token>& tokens) -> void;
 
-class tokenstream : public anzu::peekstream<std::vector<token>>
+class tokenstream : public anzu::peekstream<std::vector<lex_token>>
 {
 public:
-    tokenstream(const std::vector<token>& tokens);
+    tokenstream(const std::vector<lex_token>& tokens);
     auto consume_maybe(std::string_view text) -> bool;
-    auto consume_only(std::string_view text) -> token;
+    auto consume_only(std::string_view text) -> lex_token;
     auto consume_i64() -> std::int64_t;
     auto consume_u64() -> std::uint64_t;
 
@@ -82,11 +135,13 @@ public:
     auto peek(std::string_view text) -> bool;
     auto peek_next(std::string_view text) -> bool;
 };
+
+using token = lex_token;
     
 }
 
-template <> struct std::formatter<anzu::token_type> : std::formatter<std::string> {
-    auto format(const anzu::token_type& tt, auto& ctx) {
+template <> struct std::formatter<anzu::lex_token_type> : std::formatter<std::string> {
+    auto format(const anzu::lex_token_type& tt, auto& ctx) {
         return std::formatter<std::string>::format(to_string(tt), ctx);
     }
 };
