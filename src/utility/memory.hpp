378d2be3a529115780ae57c10289ce3cf71b04f3
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2024-07-15 22:45:43 +0100

    Remove unused functions

--- src/utility/memory.hpp
+++ src/utility/memory.hpp
@@ -7,18 +7,12 @@
 
 namespace anzu {
 
-inline auto pop_n(std::vector<std::byte>& vec, std::size_t count) -> void
-{
-    vec.resize(vec.size() - count);
-}
-
 template <typename T>
 auto push_value_single(std::vector<std::byte>& mem, const T& value) -> void
 {
-    const auto bytes = std::bit_cast<std::array<std::byte, sizeof(T)>>(value);
-    for (const auto& b : bytes) {
-        mem.push_back(b);
-    }
+    const auto old_size = mem.size();
+    mem.resize(mem.size() + sizeof(T));
+    std::memcpy(&mem[old_size], &value, sizeof(T));
 }
 
 template <typename... Ts>
@@ -30,34 +24,9 @@ auto push_value(std::vector<std::byte>& mem, Ts&&... values) -> std::size_t
 }
 
 template <typename T>
-auto pop_value(std::vector<std::byte>& mem) -> T
-{
-    auto ret = T{};
-    std::memcpy(&ret, &mem[mem.size() - sizeof(T)], sizeof(T));
-    mem.resize(mem.size() - sizeof(T));
-    return ret;
-}
-
-template <typename T>
-auto read_top(std::vector<std::byte>& mem) -> T
-{
-    auto ret = T{};
-    std::memcpy(&ret, &mem[mem.size() - sizeof(T)], sizeof(T));
-    return ret;
-}
-
-template <typename T>
 auto write_value(std::vector<std::byte>& mem, std::size_t ptr, const T& value) -> void
 {
     std::memcpy(&mem[ptr], &value, sizeof(T));
-}
-
-template <typename T>
-auto read_value(std::vector<std::byte>& mem, std::size_t ptr) -> T
-{
-    auto ret = T{};
-    std::memcpy(&ret, &mem[ptr], sizeof(T));
-    return ret;
 }
 
 }
