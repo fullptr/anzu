ae68bd33f45654570c40b405b0a4eba3ac227349
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2024-07-22 22:11:14 +0100

    Fix parsing of the arena type to allow it in function definitions

--- src/parser.cpp
+++ src/parser.cpp
@@ -326,23 +326,19 @@ auto parse_expression(tokenstream& tokens) -> node_expr_ptr
 auto parse_simple_type(tokenstream& tokens) -> type_name
 {
     const auto tok = tokens.consume();
-    if (tok.text == "null") return type_fundamental::null_type;
-    if (tok.text == "bool") return type_fundamental::bool_type;
-    if (tok.text == "char") return type_fundamental::char_type;
-    if (tok.text == "i32")  return type_fundamental::i32_type;
-    if (tok.text == "i64")  return type_fundamental::i64_type;
-    if (tok.text == "u64")  return type_fundamental::u64_type;
-    if (tok.text == "f64")  return type_fundamental::f64_type;
+    if (tok.text == "null")  return type_fundamental::null_type;
+    if (tok.text == "bool")  return type_fundamental::bool_type;
+    if (tok.text == "char")  return type_fundamental::char_type;
+    if (tok.text == "i32")   return type_fundamental::i32_type;
+    if (tok.text == "i64")   return type_fundamental::i64_type;
+    if (tok.text == "u64")   return type_fundamental::u64_type;
+    if (tok.text == "f64")   return type_fundamental::f64_type;
+    if (tok.text == "arena") return type_arena{};
     return {type_struct{ .name=std::string{tok.text} }};
 }
 
 auto parse_type_inner(tokenstream& tokens) -> type_name
 {
-    // Arena
-    if (tokens.consume_maybe(token_type::kw_arena)) {
-        return type_arena{};
-    }
-
     // Const
     if (tokens.consume_maybe(token_type::kw_const)) {
         auto ret = type_const{};
