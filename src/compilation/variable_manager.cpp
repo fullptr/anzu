f1e9579b13150fdfa8f815001871e1ae7d31c373
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2024-07-24 03:41:00 +0100

    Clean up the variable manager, make scope a simple struct

--- src/compilation/variable_manager.cpp
+++ src/compilation/variable_manager.cpp
@@ -15,7 +15,7 @@ auto delete_arena(std::vector<std::byte>& program, const variable& arena)
 
 auto delete_arenas_in_scope(std::vector<std::byte>& program, const scope& scope)
 {
-    for (const auto& variable : scope.d_variables | std::views::reverse) {
+    for (const auto& variable : scope.varibles | std::views::reverse) {
         if (variable.type == arena_type()) {
             delete_arena(program, variable);
         }
@@ -24,94 +24,45 @@ auto delete_arenas_in_scope(std::vector<std::byte>& program, const scope& scope)
 
 }
 
-scope::scope(const scope_info& info, std::size_t start_location)
-    : d_info{info}
-    , d_variables{}
-    , d_start(start_location)
-    , d_next(start_location)
-{}
-
-auto scope::scope_size() const -> std::size_t { return d_next - d_start; }
-
-auto scope::find(const std::string& name) const -> std::optional<variable>
-{
-    for (const auto& var : d_variables) {
-        if (var.name == name) return var;
-    }
-    return std::nullopt;
-}
-
-auto scope::next_location() -> std::size_t { return d_next; }
-
-auto variable_manager::push_scope() -> void
+auto variable_manager::new_scope() -> scope_guard
 {
     d_scopes.emplace_back(
         simple_scope{},
-        d_scopes.empty() ? 0 : d_scopes.back().next_location()
+        d_scopes.empty() ? 0 : d_scopes.back().next
     );
-}
-
-auto variable_manager::new_scope() -> scope_guard
-{
-    push_scope();
     return scope_guard{*this};
 }
 
 auto variable_manager::new_function_scope(const type_name& return_type) -> scope_guard
 {
-    push_function_scope(return_type);
+    d_scopes.emplace_back(function_scope{return_type}, 0);
     return scope_guard{*this};
 }
 
 auto variable_manager::new_loop_scope() -> scope_guard
 {
-    push_loop_scope();
+    d_scopes.emplace_back(loop_scope{}, d_scopes.back().next);
     return scope_guard{*this};
-}
-
-auto variable_manager::push_function_scope(const type_name& return_type) -> void
-{
-    d_scopes.emplace_back(function_scope{return_type}, 0);
-}
-
-auto variable_manager::push_loop_scope() -> void
-{
-    d_scopes.emplace_back(loop_scope{}, d_scopes.back().next_location());
-}
-
-auto variable_manager::pop_scope() -> std::size_t
-{
-    panic_if(d_scopes.empty(), "Tried to pop a scope, but there are none!");
-    const auto size = d_scopes.back().scope_size();
-    d_scopes.pop_back();
-    return size;
 }
 
 auto variable_manager::declare(
     const std::string& name, const type_name& type, std::size_t size
 ) -> bool
 {
-    const auto in_function = [&] {
-        for (const auto& scope : d_scopes | std::views::reverse) {
-            if (scope.is<function_scope>()) return true;
-        }
-        return false;
-    }();
-
     auto& scope = d_scopes.back();
-    for (const auto& var : scope.d_variables) {
+    for (const auto& var : scope.varibles) {
         if (var.name == name) return false;
     }
-    scope.d_variables.emplace_back(std::string{name}, type, scope.d_next, size, in_function);
-    scope.d_next += size;
+    scope.varibles.emplace_back(name, type, scope.next, size, in_function());
+    scope.next += size;
     return true;
 }
 
 auto variable_manager::find(const std::string& name) const -> std::optional<variable>
 {
     for (const auto& scope : d_scopes | std::views::reverse) {
-        if (const auto v = scope.find(name); v.has_value()) {
-            return v;
+        for (const auto& var : scope.varibles) {
+            if (var.name == name) return var;
         }
     }
     return std::nullopt;
@@ -120,7 +71,7 @@ auto variable_manager::find(const std::string& name) const -> std::optional<vari
 auto variable_manager::in_loop() const -> bool
 {
     for (const auto& scope : d_scopes | std::views::reverse) {
-        if (scope.is<loop_scope>()) return true;
+        if (std::holds_alternative<loop_scope>(scope.info)) return true;
     }
     return false;
 }
@@ -128,7 +79,7 @@ auto variable_manager::in_loop() const -> bool
 auto variable_manager::in_function() const -> bool
 {
     for (const auto& scope : d_scopes | std::views::reverse) {
-        if (scope.is<function_scope>()) return true;
+        if (std::holds_alternative<function_scope>(scope.info)) return true;
     }
     return false;
 }
@@ -136,8 +87,8 @@ auto variable_manager::in_function() const -> bool
 auto variable_manager::get_loop_info() -> loop_scope&
 {
     for (auto& scope : d_scopes | std::views::reverse) {
-        if (scope.is<loop_scope>()) {
-            return scope.as<loop_scope>();
+        if (auto loop = std::get_if<loop_scope>(&scope.info)) {
+            return *loop;
         }
     }
     panic("could not get loop info, not in a loop!");
@@ -146,8 +97,8 @@ auto variable_manager::get_loop_info() -> loop_scope&
 auto variable_manager::get_function_info() -> function_scope&
 {
     for (auto& scope : d_scopes | std::views::reverse) {
-        if (scope.is<function_scope>()) {
-            return scope.as<function_scope>();
+        if (auto func = std::get_if<function_scope>(&scope.info)) {
+            return *func;
         }
     }
     panic("could not get function info, not in a function!");
@@ -163,8 +114,8 @@ auto variable_manager::handle_loop_exit() -> void
     auto pop_size = std::size_t{0};
     for (const auto& scope : d_scopes | std::views::reverse) {
         delete_arenas_in_scope(*d_program, scope);
-        pop_size += scope.d_next - scope.d_start;
-        if (scope.is<loop_scope>()) break;
+        pop_size += scope.next - scope.start;
+        if (std::holds_alternative<loop_scope>(scope.info)) break;
     }
     push_value(*d_program, op::pop, pop_size);
 }
@@ -175,7 +126,7 @@ auto variable_manager::handle_function_exit() -> void
 {
     for (const auto& scope : d_scopes | std::views::reverse) {
         delete_arenas_in_scope(*d_program, scope);
-        if (scope.is<function_scope>()) break;
+        if (std::holds_alternative<function_scope>(scope.info)) break;
     }
 }
 
@@ -188,9 +139,13 @@ scope_guard::~scope_guard() {
     delete_arenas_in_scope(*d_manager->d_program, d_manager->d_scopes.back());
 
     // Then pop all local variables
-    const auto scope_size = d_manager->pop_scope();
-    if (scope_size > 0) {
-        push_value(*d_manager->d_program, op::pop, scope_size);
+    panic_if(d_manager->d_scopes.empty(), "Tried to pop a scope, but there are none!");
+    const auto& scope = d_manager->d_scopes.back();
+    const auto size = scope.next - scope.start;
+    d_manager->d_scopes.pop_back();
+
+    if (size > 0) {
+        push_value(*d_manager->d_program, op::pop, size);
     }    
 }
 
