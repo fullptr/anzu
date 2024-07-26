e216713bb2af7245cf69d370a9349947a50a3d24
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2024-07-26 02:21:02 +0100

    Fix build error in last PR

--- src/object.cpp
+++ src/object.cpp
@@ -216,7 +216,7 @@ auto arena_type() -> type_name
 
 auto string_literal_type() -> type_name
 {
-    return char_type().add_const().add_span()
+    return char_type().add_const().add_span();
 }
 
 auto make_type(const std::string& name) -> type_name
