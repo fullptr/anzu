e52a5c758e4d5c3707e776d38b3936f3f486ee28
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2024-08-06 00:49:21 +0100

    Add templates_of to type_manager

--- src/compilation/type_manager.hpp
+++ src/compilation/type_manager.hpp
@@ -12,17 +12,26 @@ struct type_field
     auto operator==(const type_field&) const -> bool = default;
 };
 
+using type_fields = std::vector<type_field>;
+using type_templates = std::unordered_map<std::string, type_name>;
+
+struct type_info
+{
+    type_fields fields;
+    type_templates templates;
+};
+
 class type_manager
 {
-    using type_fields = std::vector<type_field>;
-    std::unordered_map<type_name, type_fields, type_hash> d_classes;
+    std::unordered_map<type_name, type_info, type_hash> d_classes;
 
 public:
-    auto add(const type_name& name, const type_fields& fields) -> bool;
+    auto add(const type_name& name, const type_fields& fields, const type_templates& templates = {}) -> bool;
     auto contains(const type_name& t) const -> bool;
 
     auto size_of(const type_name& t) const -> std::size_t;
     auto fields_of(const type_name& t) const -> type_fields;
+    auto templates_of(const type_name& t) const -> type_templates;
 };
 
 }
